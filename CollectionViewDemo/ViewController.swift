75f33237a04fcb8a5aad14cd1310c8f5daead3f2
Robert M. Ryan <robert.ryan@mindspring.com>
2017-07-14 20:53:03 -0700

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Robert Ryan on 7/14/17.
+//  Copyright © 2017 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
