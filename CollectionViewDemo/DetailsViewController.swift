a117723065e1a66054e1eb51b0f223999a79814e
Robert M. Ryan <robert.ryan@mindspring.com>
2017-07-14 21:43:06 -0700

    Add labels to detail view controller

--- CollectionViewDemo/DetailsViewController.swift
+++ CollectionViewDemo/DetailsViewController.swift
@@ -12,10 +12,16 @@ class DetailsViewController: UIViewController {
 
     var person: Person!
     
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print(person)
+        
+        firstNameLabel.text = person.firstName
+        lastNameLabel.text  = person.lastName
     }
 
 }
